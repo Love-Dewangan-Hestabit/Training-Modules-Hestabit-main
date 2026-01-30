7e588085fa499f504244f3d25c4fb6fa411e0507
Love Dewangan Hestabit <loved.hestabit@gmail.com>
2026-01-30 17:41:10 +0530

    add changes to product controller.

--- Week 4 Advanced Backend Engineering/Backend Project Directory/src/controllers/product.controller.js
+++ Week 4 Advanced Backend Engineering/Backend Project Directory/src/controllers/product.controller.js
@@ -1,11 +1,19 @@
 import productService from "../services/product.service.js";
+import { emailQueue } from "../jobs/email.job.js";
+import logger from "../utils/logger.js";
 
 class ProductController {
   async getProducts(req, res, next) {
     try {
+      logger.info("Controller: getProducts called", {
+        requestId: req.requestId,
+      });
+
       const products = await productService.getProducts(
         req.validatedQuery || req.query,
+        req.requestId,
       );
+
       res.json({ success: true, data: products });
     } catch (err) {
       next(err);
@@ -14,8 +22,39 @@ class ProductController {
 
   async deleteProduct(req, res, next) {
     try {
-      const product = await productService.deleteProduct(req.params.id);
+      logger.info("Controller: deleteProduct called", {
+        requestId: req.requestId,
+        productId: req.params.id,
+      });
+
+      const product = await productService.deleteProduct(
+        req.params.id,
+        req.requestId,
+      );
+
       res.json({ success: true, data: product });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async notifyProduct(req, res, next) {
+    try {
+      const job = await emailQueue.add("send-email", {
+        to: "user@example.com",
+        subject: "Product Notification",
+      });
+
+      logger.info("Controller: email job queued", {
+        requestId: req.requestId,
+        jobId: job.id,
+      });
+
+      res.json({
+        success: true,
+        message: "Email notification queued",
+        jobId: job.id,
+      });
     } catch (err) {
       next(err);
     }
