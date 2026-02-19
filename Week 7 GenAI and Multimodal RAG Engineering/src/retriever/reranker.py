3a27a5f96e118fb91d70d0a7147dbf4a2a327eeb
Love Dewangan Hestabit <loved.hestabit@gmail.com>
2026-02-19 18:51:06 +0530

    add reranker file

---
+++ Week 7 GenAI and Multimodal RAG Engineering/src/retriever/reranker.py
@@ -0,0 +1,16 @@
+from sentence_transformers import CrossEncoder
+
+class Reranker:
+    def __init__(self):
+        self.model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+    def rerank(self, query, documents, top_k=5):
+        pairs = [(query, doc["text"]) for doc in documents]
+        scores = self.model.predict(pairs)
+
+        for i, score in enumerate(scores):
+            documents[i]["rerank_score"] = float(score)
+
+        documents = sorted(documents, key=lambda x: x["rerank_score"], reverse=True)
+
+        return documents[:top_k]       
